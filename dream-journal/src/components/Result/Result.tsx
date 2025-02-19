7a664a36d54f3238e48f0e4d9f69e8992938229f
Nasibeh Ashtari <n.ashtari@mydigipay.com>
2025-02-19 22:06:51 +0330

    [ADD] result component

--- dream-journal/src/components/Result/Result.tsx
+++ dream-journal/src/components/Result/Result.tsx
@@ -1,4 +1,10 @@
 import { Dream } from "../../types/dream.ts";
+
+import Button from "../Button/Button.tsx";
+
+import MingcuteEdit2Line from "../../icons/MingcuteEdit2Line.tsx";
+import MingcuteDelete2Line from "../../icons/MingcuteDelete2Line.tsx";
+
 import styles from "./Result.module.css";
 
 const dreams: Dream[] = [
@@ -27,9 +33,19 @@ const dreams: Dream[] = [
 
 export default function Result() {
   return (
-    <ul className="result">
+    <ul className={styles.result}>
       {dreams.map((dream) => (
-        <li key={dream.id}>{dream.title}</li>
+        <li key={dream.id}>
+          <div className={styles.title}> {dream.title}</div>
+          <div className={styles.actions}>
+            <Button variant="ghost" size="small" shape="square">
+              <MingcuteEdit2Line />
+            </Button>
+            <Button color="danger" variant="ghost" size="small" shape="square">
+              <MingcuteDelete2Line />
+            </Button>
+          </div>
+        </li>
       ))}
     </ul>
   );
