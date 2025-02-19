86be40896f3bc2b245418edd67f390b2f7a6877e
Nasibeh Ashtari <n.ashtari@mydigipay.com>
2025-02-19 11:59:42 +0330

    [ADD] header component

---
+++ dream-journal/src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import style from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={style.header}>
+      <h1>Dream Journal</h1>
+    </header>
+  );
+}
