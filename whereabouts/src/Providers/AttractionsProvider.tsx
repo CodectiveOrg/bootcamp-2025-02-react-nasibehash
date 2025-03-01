529514b5c92cbc96fcd9903189552fae95adf36b
Nasibeh ashtari <57233906+nasibehash@users.noreply.github.com>
2025-03-01 10:06:10 +0330

    Task 6 (#7)
    * [ADD] whereabouts project
    
    * [ADD] root layout, header and footer components
    
    * [ADD] attractions list and list items components
    
    * [ADD] filters and attractions context
    
    * [ADD] attraction detail
    
    * [DELETE] about styles import
    
    * [ADD] whereabout demo url to README.md
    
    * [CHANGE] env url
    
    * [DELETE] filters log in tagfilter component
    
    * [ADD] env Url
    
    ---------
    
    Co-authored-by: Nasibeh Ashtari <n.ashtari@mydigipay.com>

---
+++ whereabouts/src/Providers/AttractionsProvider.tsx
@@ -0,0 +1,46 @@
+import {
+  PropsWithChildren,
+  ReactElement,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+import { FiltersContext } from "../context/FiltersContext.tsx";
+import { AttractionsContext } from "../context/AttractionsContext.tsx";
+
+import { Attraction } from "../types/attraction.ts";
+
+type Props = PropsWithChildren;
+
+function AttractionsProvider({ children }: Props): ReactElement {
+  const { filters } = useContext(FiltersContext);
+
+  const [filteredAttractions, setFilteredAttractions] = useState<Attraction[]>(
+    [],
+  );
+
+  useEffect(() => {
+    const fetchAttractions = async (): Promise<void> => {
+      const params = new URLSearchParams();
+      filters.tags.forEach((tag) => params.append("tag", tag.id.toString()));
+
+      const response = await fetch(
+        `${import.meta.env.VITE_API_BASE_URL}/attraction?${params.toString()}`,
+      );
+      const data = await response.json();
+
+      setFilteredAttractions(data);
+    };
+
+    fetchAttractions().then();
+  }, [filters]);
+
+  return (
+    <AttractionsContext.Provider value={{ filteredAttractions }}>
+      {children}
+    </AttractionsContext.Provider>
+  );
+}
+
+export default AttractionsProvider;
