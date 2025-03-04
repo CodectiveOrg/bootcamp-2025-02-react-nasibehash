ac6148f22c98c02e0497a6937f0fa26a173570f8
Nasibeh Ashtari <n.ashtari@mydigipay.com>
2025-03-04 14:02:33 +0330

    [ADD] Loading component

--- whereabouts/src/components/AttractionsList/AttractionList.tsx
+++ whereabouts/src/components/AttractionsList/AttractionList.tsx
@@ -2,6 +2,7 @@ import { ReactElement, useContext } from "react";
 import { useQuery } from "@tanstack/react-query";
 
 import AttractionListItem from "../AttractionListItem/AttractionListItem.tsx";
+import Loading from "../Loading/Loading.tsx";
 
 import { fetchAttractions } from "../../api/fetch-attractions.ts";
 
@@ -12,19 +13,30 @@ import styles from "./AttractionList.module.css";
 export default function AttractionList(): ReactElement {
   const { filters } = useContext(FiltersContext);
 
-  const { data: attractions, isFetching } = useQuery({
+  const { data, isError, isLoading, isFetching, error } = useQuery({
     queryKey: ["attractions", filters],
     queryFn: () => fetchAttractions(filters),
     initialData: [],
   });
 
-  if (isFetching) {
-    return <>"در حال بارگذاری ..."</>;
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    return <>Error: {error ? error.message : "Unexpected Error!"}</>;
+  }
+
+  if (!data) {
+    return <>There is no data</>;
   }
 
   return (
-    <ul className={styles["attraction-list"]}>
-      {attractions.map((attraction) => (
+    <ul
+      className={styles["attraction-list"]}
+      style={{ opacity: isFetching ? "0.5" : "1" }}
+    >
+      {data.map((attraction) => (
         <AttractionListItem key={attraction.id} attraction={attraction} />
       ))}
     </ul>
